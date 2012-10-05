fdd860f7ca0fafeb49605519a3991d4c2a5bb8f8
scarrillo <shawn.carrillo+git@gmail.com>
2012-10-05 19:25:19 -0400

    default keymap and integrated theme.

--- Log4j.py
+++ Log4j.py
@@ -14,20 +14,24 @@ class Log4jCommand(sublime_plugin.WindowCommand):
 	def __init__(self, window):
 		sublime_plugin.WindowCommand.__init__(self, window) # super!
 		self.LEVELS = ["TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"]
+		self.loadSettings()
+
+	def loadSettings(self):
+		self.settings = sublime.load_settings("Log4j.sublime-settings")
+		self.custom_syntax = self.settings.get("syntax")
+		self.custom_scheme = self.settings.get("color_scheme")
+		self.log_file = self.settings.get("log_file")
 
 	def run(self):
 		sublime.active_window().show_input_panel('Filter: ', '', lambda s: self.doInput(s), None, None)
 
 	def doInput(self, filter):
 		panel_name = 'log4j'
-		custom_syntax = 'Packages/Log4j/Log4j.tmLanguage'
-		custom_scheme = 'Packages/Log4j/Log4j.tmTheme'
-		logFile = sublime.active_window().folders()[0] + "/log4j.log"
+		logFile = sublime.active_window().folders()[0] + "/" + self.log_file
 
-		self.init_view(panel_name, custom_syntax, custom_scheme)
+		self.init_view(panel_name, self.custom_syntax, self.custom_scheme)
 		self.initFilter(filter)
 		self.initTail(logFile)
-		#self.end_view(custom_syntax) # Need to leave open for threaded use
 
 	def initFilter(self, filter):
 		self.level = ""
@@ -96,28 +100,17 @@ class Log4jCommand(sublime_plugin.WindowCommand):
 		self.output_view.end_edit(edit)
 		self.output_view.set_read_only(True)
 
-	def init_view(self, name, syn, scheme=None):
+	def init_view(self, name, syntax=None, scheme=None):
 		# only use "get_output_panel" once, otherwise sublime will
 		# recreate a new one and you will lose any existing text
 		if not hasattr(self, 'output_view'):
 			self.output_view = self.window.get_output_panel(name)
-			self.output_view.set_syntax_file(syn)
+			self.output_view.set_syntax_file(syntax)
+			#Seems to be automatically pulling this
+			#self.output_view.settings().set("color_scheme", scheme)
 		
-		#self.output_view.set_read_only(False)
 		self.edit_clear()
 		sublime.active_window().run_command("show_panel", {"panel": "output."+name})
-
-		# if scheme is not None:
-		# 	if not hasattr(self, 'output_view_exec'):
-		# 		self.output_view_exec = self.window.get_output_panel("exec")
-		# 	self.output_view_exec.settings().set("color_scheme", scheme)
-		# 	self.output_view_exec.set_syntax_file(syn)
-		# 	sublime.active_window().run_command("show_panel", {"panel": "output.exec"})
-
-	#def end_view(self, syn):
-		#self.output_view.set_read_only(True)
-		#self.output_view.show_at_center(self.output_view.size())
-		#self.output_view.set_syntax_file(syn)
 
 	#""" Sublime sometimes invokes this when reloading the plugin """
 	#def __del__(self):
